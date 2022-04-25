21401e98487e86feb107e4edad9d9c12bfd0d5a2
Dan Ryan Huynh <89366190+Danryanh7@users.noreply.github.com>
2022-04-25 11:13:09 -0400

    Time: 1062 ms (33.77%), Space: 28 MB (41.02%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -3,7 +3,9 @@ class Solution:
         max_sub = nums[0]
         local_max = 0
         
-        for i in range(len(nums)):
-            local_max = max(nums[i], local_max + nums[i])
-            max_sub = max(local_max, max_sub)
+        for n in nums:
+            if local_max < 0:
+                local_max = 0
+            local_max += n
+            max_sub = max(max_sub, local_max)
         return max_sub
