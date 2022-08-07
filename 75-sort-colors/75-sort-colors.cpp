e200c33392295f34acbbe76b10dd3fe73e520f84
Dan Ryan Huynh <89366190+Danryanh7@users.noreply.github.com>
2022-08-06 22:02:24 -0400

    Time: 8 ms (11.94%), Space: 8.3 MB (23.78%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        if (size(nums) < 2) return;
-        for (int i = 0; i <= size(nums)-1; ++i) {
-            for (int j = 0; j <= size(nums)-2; ++j) {
+        int sizeNums = size(nums);
+        if (sizeNums < 2) return;
+        for (int i = 0; i <= sizeNums-1; ++i) {
+            for (int j = 0; j <= sizeNums-2; ++j) {
                 if (nums[j] > nums[j+1]) {
                     int temp = nums[j];
                     nums[j] = nums[j+1];
