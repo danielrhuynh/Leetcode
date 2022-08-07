300606614eb38c8858eaaf473c7bf0cf1210d326
Dan Ryan Huynh <89366190+Danryanh7@users.noreply.github.com>
2022-08-06 22:04:23 -0400

    Time: 4 ms (45.02%), Space: 8.2 MB (70.34%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -6,9 +6,7 @@ public:
         for (int i = 0; i <= sizeNums-1; ++i) {
             for (int j = 0; j <= sizeNums-2; ++j) {
                 if (nums[j] > nums[j+1]) {
-                    int temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = temp;
+                    swap(nums[j], nums[j+1]);
                 }
             }
         }
