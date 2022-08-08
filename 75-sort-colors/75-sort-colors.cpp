f2891f5fbbe59280d463805600236f0287f19b14
Dan Ryan Huynh <89366190+Danryanh7@users.noreply.github.com>
2022-08-07 22:24:14 -0400

    Time: 2 ms (66.41%), Space: 8.2 MB (90.52%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // Using selection sort
-        int sizeArray = nums.size();
-        for (int i = 0; i < sizeArray-1; i++) {
-            int minIndex = i;
-            // find min value
-            for (int j = i+1; j < sizeArray; j++) {
-                if (nums[j] < nums[minIndex]) {
-                    minIndex = j;
-                }
-            }
-            // Perform swap
-            swap(nums[i], nums[minIndex]);
+        priority_queue <int> minHeap;
+        for (int i = 0; i < nums.size(); i++) {
+            minHeap.push(nums[i]);
         }
+        for (int i = 0; i < nums.size(); i++) {
+            nums[nums.size()-i-1] = minHeap.top();
+            minHeap.pop();
+        }
+        
     }
 };
