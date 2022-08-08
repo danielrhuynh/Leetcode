dca6acf64aeaf9025ddfa3461c9945e649f0ac08
Dan Ryan Huynh <89366190+Danryanh7@users.noreply.github.com>
2022-08-07 22:06:32 -0400

    Time: 3 ms (64.07%), Space: 8.4 MB (23.78%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        int sizeArray = nums.size();
-        for (int i = 0; i < sizeArray; i++) {
-            for (int j = 0; j < sizeArray-1; j++) {
-                if (nums[j] > nums[j+1]) {
-                    int temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = temp;
-                }
+    void bubbleSort(vector<int>&nums, int n) {
+        if (n == 1) {
+            return;
+        }
+        int count = 0;
+        for (int i = 0; i < n-1; i++) {
+            if (nums[i] > nums[i+1]) {
+                swap(nums[i], nums[i+1]);
+                ++count;
             }
         }
+        if (count == 0) {
+                return;
+            }
+        bubbleSort(nums, n-1);
+    }
+    void sortColors(vector<int>& nums) {
+        bubbleSort(nums, nums.size());
     }
 };
