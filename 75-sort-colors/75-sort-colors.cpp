85edc5c800a60c6bab6384af1956b00d381469ff
Dan Ryan Huynh <89366190+Danryanh7@users.noreply.github.com>
2022-08-07 22:28:15 -0400

    Time: 3 ms (64.07%), Space: 8.2 MB (70.34%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        // Using heap sort
         priority_queue <int> minHeap;
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size(); i++)
             minHeap.push(nums[i]);
-        }
         for (int i = 0; i < nums.size(); i++) {
             nums[nums.size()-i-1] = minHeap.top();
             minHeap.pop();
