4940103945f7e5dce25bf2f13096bcea3727c5de
Dan Ryan Huynh <89366190+Danryanh7@users.noreply.github.com>
2023-01-31 17:23:42 -0500

    Time: 557 ms (99.48%), Space: 54.4 MB (65.29%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.scala
@@ -0,0 +1,22 @@
+import scala.collection.mutable.HashMap
+
+object Solution {
+    def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+        var seen = new HashMap[Int, Int]()
+        
+        // For loop in scala where zipWithIndex is like enumerate and the arrow is a generator for the loop range
+        for ((element, index) <- nums.zipWithIndex) {
+            // Find required int
+            // Check if required is in seen
+            // If True return accessed index, and current index
+            var required: Int = target-element
+            if (seen.contains(required)) {
+                return Array(index, seen.get(required).get)
+            }
+            // If not found place element and index in hashMap in the form element: index
+            seen.put(element, index)
+        }
+        // Return short hand
+        Array(0,0)
+    }
+}
