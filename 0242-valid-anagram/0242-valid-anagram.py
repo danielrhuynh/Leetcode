24775d258cdd0a364ebba52b1baaf1f4cd04baba
Dan Ryan Huynh <89366190+Danryanh7@users.noreply.github.com>
2023-05-03 19:24:10 -0400

    Time: 69 ms (10.71%), Space: 17 MB (5.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        # return Counter(s) == Counter(t)
+        if len(s) != len(t):
+            return False
+        sMap = {}
+        tMap = {}
+        
+        for i in range(len(s)):
+            if s[i] not in sMap:
+                sMap[s[i]] = 1
+            else:
+                sMap[s[i]] += 1
+        for i in range(len(t)):
+            if t[i] not in tMap:
+                tMap[t[i]] = 1
+            else:
+                tMap[t[i]] += 1
+        return sMap == tMap
+        
