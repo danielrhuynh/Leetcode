142038f0514882c900d8985169bae67e1466eabd
Dan Ryan Huynh <89366190+Danryanh7@users.noreply.github.com>
2023-05-15 19:10:46 -0400

    Time: 172 ms (46.31%), Space: 18 MB (7.70%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,12 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         # Store count in hashmap
-        seen = {}
-        
-        for num in nums:
-            if num not in seen:
-                seen[num] = 1
-            else:
-                seen[num] += 1
+        seen = Counter(nums)
         return max(seen, key=lambda x: seen[x])
         
