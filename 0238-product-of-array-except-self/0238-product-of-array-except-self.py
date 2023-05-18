7c4053c39ff68507d07b0393ecdaf0cff29e1cea
Dan Ryan Huynh <89366190+Danryanh7@users.noreply.github.com>
2023-05-18 19:49:09 -0400

    Time: 231 ms (76.66%), Space: 23.3 MB (46.30%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,19 +1,19 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # Seems like a permutations questions
-        # O(n) only run through the array once
-        # Prefix and postfix
-        res = [1]*len(nums)
-        prefix = 1
+        # Would be really easy if you could use the division operator
+        # Use a prefix postfix approach
+        res = [1] * len(nums)
+        product = 1
+        # Getting prefix array using nums
         for i in range(len(nums)):
-            res[i] *= prefix
-            prefix *= nums[i]
-        postfix = 1
+            res[i] *= product
+            product *= nums[i]
+        product = 1
+        # Second pass applying postfix
         for i in range(len(nums)-1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
+            res[i] *= product
+            product *= nums[i]
         return res
-            
             
         
             
