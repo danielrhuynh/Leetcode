ac77d611381891353c0f427a8018d59ef3168556
Dan Ryan Huynh <89366190+Danryanh7@users.noreply.github.com>
2023-05-23 19:42:27 -0400

    Time: 94 ms (16.14%), Space: 16.5 MB (8.05%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,20 +1,20 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        
-        
-        def dfs(i, cur, total):
-            if total == target:
-                res.append(cur.copy())
-                return
-            if i >= len(candidates) or total > target:
-                return
-            
-            cur.append(candidates[i])
-            dfs(i, cur, total+candidates[i])
-            
-            cur.pop()
-            dfs(i+1, cur, total)
+        def dfs(i, curr, total_sum):
+                # Base cases
+                if total_sum == target:
+                    res.append(curr.copy())
+                    return
+                if i >= len(candidates) or total_sum > target:
+                    return
+                # Recursive step, 2 decisions to make
+                curr.append(candidates[i])
+                # Decision where we include the current value
+                dfs(i, curr, total_sum + candidates[i])
+                # Clean up current decision and travel down branch where the current value cannot be chosen anymore
+                curr.pop()
+                dfs(i+1, curr, total_sum)
         dfs(0, [], 0)
         return res
             
