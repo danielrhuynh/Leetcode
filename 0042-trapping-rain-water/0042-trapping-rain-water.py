8e09c69fd99deaf2a5a9f7046485ba461446de22
Dan Ryan Huynh <89366190+Danryanh7@users.noreply.github.com>
2023-06-08 19:44:09 -0400

    Time: 182 ms (6.52%), Space: 18.9 MB (12.62%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -0,0 +1,31 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        max_l = [height[0]]
+        max_r = [height[-1]]
+        min_l_r = []
+        local_max_l = 0
+        local_max_r = 0
+        res = 0
+        
+        for i in range(1, len(height)):
+            max_l.append(max(height[i], max_l[-1]))
+        for i in range(len(height)-2, -1, -1):
+            max_r.append(max(height[i], max_r[-1]))
+        for i in range(len(height)):
+            min_l_r.append(min(max_l[i], max_r[len(height)-1-i]))
+        
+        print(max_l)
+        print(max_r)
+        print(min_l_r)
+                                     
+        # Formula for water contained = min(l,r) - height[i]
+        for i in range(len(height)):
+            val = max((min_l_r[i] - height[i]), 0)
+            res += val
+        return res
+                                
+        
+            
+            
+            
+        
