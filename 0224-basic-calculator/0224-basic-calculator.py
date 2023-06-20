70da8070d895e9a583b12746a1c26281e54c76f0
Dan Ryan Huynh <89366190+Danryanh7@users.noreply.github.com>
2023-06-20 19:54:08 -0400

    Time: 99 ms (56.75%), Space: 17.8 MB (37.52%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -0,0 +1,35 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        res = 0
+        curr = 0
+        sign = 1
+        stack = []
+        for i in range(len(s)):
+            # Checking for a digit
+            if s[i].isdigit():
+                # moves curr one decimal place to the left and add new digit
+                curr = curr*10+int(s[i])
+            # Here we finished proccessing the previous thing
+            elif s[i] == '+' or s[i] == '-':
+                res += sign*curr
+                # Now reset variables for next iteration
+                sign = 1 if s[i] == '+' else -1
+                curr = 0
+            elif s[i] == '(':
+                stack.append(res)
+                stack.append(sign)
+                # Now we start a new iteration
+                sign = 1
+                res = 0
+            elif s[i] == ')':
+                res += sign*curr
+                sign = stack.pop()
+                sum = stack.pop()
+                res *= sign
+                res += sum
+                
+                # Finished processing, next iteration
+                curr = 0
+        return res + sign*curr
+            
+            
