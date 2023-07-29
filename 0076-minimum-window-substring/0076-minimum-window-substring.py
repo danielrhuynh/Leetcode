c761c435dd4ba78c56caa345004f6aadbbc917e3
Dan Ryan Huynh <89366190+Danryanh7@users.noreply.github.com>
2023-07-28 20:39:58 -0400

    Time: 107 ms (98.27%), Space: 17.2 MB (24.55%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,27 +1,35 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if t == '': return ''
-        tCount = Counter(t)
-        window = {}
+        if t == "": return ""
         
-        have, need = 0, len(tCount)
-        res, resLen = [-1, -1], 10**5+1
+        countT, window = Counter(t), {}
+        
+        have, need = 0, len(countT)
+        
+        res, resLen = [-1, -1], float("infinity")
         l = 0
+        
+        # Iterate over s
         for r in range(len(s)):
-            char = s[r]
-            window[char] = window.get(char, 0) +1
+            c = s[r]
+            # Add c to curr
+            window[c] = 1 + window.get(c, 0)
             
-            if char in tCount and window[char] == tCount[char]:
+            # If c is in target and values are exactly equal
+            if c in countT and window[c] == countT[c]:
                 have += 1
+            # If our condiiton is met
             while have == need:
-                if r-l+1 < resLen:
-                    resLen = r-l+1
+                # Update our result
+                if (r - l + 1) < resLen:
                     res = [l, r]
-                    
+                    resLen = (r - l + 1)
+                # pop from left of window
                 window[s[l]] -= 1
-                if s[l] in tCount and window[s[l]] < tCount[s[l]]:
+                # if we no longer satisfy a condition reduce have
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
                     have -= 1
+                # Move window
                 l += 1
         l, r = res
-        return s[l:r+1] if resLen != 10**5+1 else ''
-                    
+        return s[l:r+1] if resLen != float("infinity") else ""
