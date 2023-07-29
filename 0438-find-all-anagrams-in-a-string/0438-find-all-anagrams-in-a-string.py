0625a22356e73cecd59f86660d365f4d74c0deff
Dan Ryan Huynh <89366190+Danryanh7@users.noreply.github.com>
2023-07-28 20:12:31 -0400

    Time: 316 ms (29.61%), Space: 17.5 MB (74.85%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -3,23 +3,29 @@ class Solution:
         l, r = 0, len(p)-1
         curr = Counter(s[l:r+1])
         res= []
-        
-        
         target = Counter(p)
         
         while r < len(s):
+            # Check for res
             if curr == target:
                 res.append(l)
-                
+            
+            # Remove l from window
             if s[l] in curr:
                 if curr[s[l]] > 1:
                     curr[s[l]] -= 1
                 else:
                     curr.pop(s[l])
+            
+            # Slide window
             r += 1
             l += 1
+            
+            # Handle edge case (last iteration)
             if r >= len(s):
                 break
+            
+            # Add r to window
             if s[r] in target:
                 if s[r] not in curr:
                     curr[s[r]] = 1
