d43cc97201a68346c97b3244c53b4ea8e18a2312
Dan Ryan Huynh <89366190+Danryanh7@users.noreply.github.com>
2023-08-03 22:14:26 -0400

    Time: 54 ms (69.05%), Space: 16.8 MB (39.45%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,22 +1,29 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums)-1
+        # Basically the idea here is regular binary search with edge cases for the left and             right sorted portions
+        low, high = 0, len(nums)-1
         
-        while l <= r:
-            mid = (l+r)//2
+        while low <= high:
+            mid = (low+high)//2
             if nums[mid] == target:
                 return mid
-            #Left sorted portion
-            if nums[mid] >= nums[l]:
-                if target > nums[mid] or target < nums[l]:
-                    l = mid+1
+            # else keep searching
+            if nums[mid] >= nums[low]:
+                # We know the pivot put smaller numbers to the left
+                if target > nums[mid] or target < nums[low]:
+                    # If the target is greater than mid because things to the left
+                    # of mid are lower we know the target is not there so set low to mid+1
+                    # or if the target is less than low, since the array is pivoted it has to
+                    # be in the right side
+                    low = mid+1
                 else:
-                    r = mid-1
-            #Right sorted portion
+                    # Else we know its in the left side
+                    high = mid-1
             else:
-                if target < nums[mid] or target > nums[r]:
-                    r = mid-1
+                if target < nums[mid] or target > nums[high]:
+                    # Same intuition here we know its in the left side
+                    high = mid-1
                 else:
-                    l = mid+1
+                    # Here it is in the right side
+                    low = mid+1
         return -1
-                
