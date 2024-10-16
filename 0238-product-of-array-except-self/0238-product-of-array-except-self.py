32615b6d5e8a703e76b649499d801c522828d304
Dan Ryan Huynh <89366190+danielrhuynh@users.noreply.github.com>
2024-10-16 11:09:14 -0400

    Time: 310 ms (6.69%), Space: 26.5 MB (8.32%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,20 +1,15 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # Would be really easy if you could use the division operator
-        # Use a prefix postfix approach
-        res = [1] * len(nums)
-        product = 1
-        # Getting prefix array using nums
+        prefix = 1
+        postfix = 1
+        res = []
+        
         for i in range(len(nums)):
-            res[i] *= product
-            product *= nums[i]
-        product = 1
-        # Second pass applying postfix
+            print(i)
+            res.append(prefix)
+            prefix *= nums[i]
+            
         for i in range(len(nums)-1, -1, -1):
-            res[i] *= product
-            product *= nums[i]
+            res[i] *= postfix
+            postfix *= nums[i]
         return res
-            
-        
-            
-        
