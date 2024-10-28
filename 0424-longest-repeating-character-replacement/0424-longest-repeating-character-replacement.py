188d2bfd76eb61b793a92bd5f55660bbe34abab3
Dan Ryan Huynh <89366190+danielrhuynh@users.noreply.github.com>
2024-10-28 12:46:25 -0400

    Time: 59 ms (51.68%), Space: 16.7 MB (52.71%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,10 +1,10 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         seen = {}
-        L = 0
+        L, R = 0, 0
         res = 0
 
-        for R in range(len(s)):
+        while R < len(s):
             if s[R] in seen:
                 seen[s[R]] += 1
             else:
@@ -17,4 +17,5 @@ class Solution:
             else:
                 seen[s[L]] -= 1
                 L += 1
+            R += 1
         return res
