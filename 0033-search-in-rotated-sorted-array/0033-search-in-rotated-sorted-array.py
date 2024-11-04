80fc324130b097803daf8c3fe03eb4ded4fdf8cc
Dan Ryan Huynh <89366190+danielrhuynh@users.noreply.github.com>
2024-11-03 19:03:17 -0500

    Time: 2 ms (5.07%), Space: 17.1 MB (14.46%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,29 +1,21 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # Basically the idea here is regular binary search with edge cases for the left and             right sorted portions
-        low, high = 0, len(nums)-1
-        
-        while low <= high:
-            mid = (low+high)//2
-            if nums[mid] == target:
-                return mid
-            # else keep searching
-            if nums[mid] >= nums[low]:
-                # We know the pivot put smaller numbers to the left
-                if target > nums[mid] or target < nums[low]:
-                    # If the target is greater than mid because things to the left
-                    # of mid are lower we know the target is not there so set low to mid+1
-                    # or if the target is less than low, since the array is pivoted it has to
-                    # be in the right side
-                    low = mid+1
+        if len(nums) == 1 and nums[0] == target:
+            return 0
+        L, R = 0, len(nums)-1
+        while L <= R:
+            M = (L + R) // 2
+            if nums[M] == target:
+                return M
+            if nums[M] >= nums[L]:
+                if target > nums[M] or target < nums[L]:
+                    L = M+1
                 else:
-                    # Else we know its in the left side
-                    high = mid-1
+                    R = M
             else:
-                if target < nums[mid] or target > nums[high]:
-                    # Same intuition here we know its in the left side
-                    high = mid-1
+                if target < nums[M] or target > nums[R]:
+                    R = M
                 else:
-                    # Here it is in the right side
-                    low = mid+1
+                    L = M+1
         return -1
+        
